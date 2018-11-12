4eb2c0cc87a1708dc2ec2ffed471346f95bd4587
Christiaan Kruger <christiaan@journeyapps.com>
2018-11-12 11:46:17 +0200

    DRY tests for client.ts

--- tests/client.test.ts
+++ tests/client.test.ts
@@ -4,7 +4,7 @@ import { Bridge } from '../src/lib/bridge';
 
 jest.mock('../src/lib/bridge');
 
-describe('client.post blocking', () => {
+describe('client.post', () => {
   let client;
   let bridgeInstance;
   const MOCK_RESULT = 'result';
@@ -17,71 +17,64 @@ describe('client.post blocking', () => {
     bridgeInstance.post.mockResolvedValue({ result: MOCK_RESULT });
   });
 
-  test('should post correctly with 0 parameters', async () => {
-    let result = await client.post(COMMAND_NAME);
+  describe('Blocking', () => {
+    test('should post correctly with 0 parameters', async () => {
+      let result = await client.post(COMMAND_NAME);
 
-    expect(result).toBe(MOCK_RESULT);
-    expect(bridgeInstance.post).toBeCalledWith(COMMAND_NAME, [], {});
-  });
+      expect(result).toBe(MOCK_RESULT);
+      expect(bridgeInstance.post).toBeCalledWith(COMMAND_NAME, [], {});
+    });
 
-  test('should post correctly with 1 parameter', async () => {
-    let result = await client.post(COMMAND_NAME, 'param1');
+    test('should post correctly with 1 parameter', async () => {
+      let result = await client.post(COMMAND_NAME, 'param1');
 
-    expect(result).toBe(MOCK_RESULT);
-    expect(bridgeInstance.post).toBeCalledWith(COMMAND_NAME, ['param1'], {});
-  });
+      expect(result).toBe(MOCK_RESULT);
+      expect(bridgeInstance.post).toBeCalledWith(COMMAND_NAME, ['param1'], {});
+    });
 
-  test('should post correctly with 2 parameters', async () => {
-    let result = await client.post(COMMAND_NAME, 'param1', 'param2');
+    test('should post correctly with 2 parameters', async () => {
+      let result = await client.post(COMMAND_NAME, 'param1', 'param2');
 
-    expect(result).toBe(MOCK_RESULT);
-    expect(bridgeInstance.post).toBeCalledWith(
-      COMMAND_NAME,
-      ['param1', 'param2'],
-      {}
-    );
-  });
-});
-
-describe('client.post non-blocking', () => {
-  let client;
-  let bridgeInstance;
-  const MOCK_RESULT = 'result';
-  const COMMAND_NAME = 'my-awesome-command';
-  beforeEach(() => {
-    (Bridge as any).mockClear();
-    client = new JourneyIFrameClient();
-    bridgeInstance = (Bridge as any).mock.instances[0];
-    // Mock the return value for bridge (using mockResolvedValue to wrap it in a promise)
-    bridgeInstance.post.mockResolvedValue({ result: MOCK_RESULT });
-  });
-
-  test('should post correctly with 0 parameters', async () => {
-    let result = await client.postNonBlocking(COMMAND_NAME);
-
-    expect(result).toBe(MOCK_RESULT);
-    expect(bridgeInstance.post).toBeCalledWith(COMMAND_NAME, [], {
-      nonBlocking: true
+      expect(result).toBe(MOCK_RESULT);
+      expect(bridgeInstance.post).toBeCalledWith(
+        COMMAND_NAME,
+        ['param1', 'param2'],
+        {}
+      );
     });
   });
+  describe('Non-blocking', () => {
+    test('should post correctly with 0 parameters', async () => {
+      let result = await client.postNonBlocking(COMMAND_NAME);
 
-  test('should post correctly with 1 parameter', async () => {
-    let result = await client.postNonBlocking(COMMAND_NAME, 'param1');
-
-    expect(result).toBe(MOCK_RESULT);
-    expect(bridgeInstance.post).toBeCalledWith(COMMAND_NAME, ['param1'], {
-      nonBlocking: true
+      expect(result).toBe(MOCK_RESULT);
+      expect(bridgeInstance.post).toBeCalledWith(COMMAND_NAME, [], {
+        nonBlocking: true
+      });
     });
-  });
 
-  test('should post correctly with 2 parameters', async () => {
-    let result = await client.postNonBlocking(COMMAND_NAME, 'param1', 'param2');
+    test('should post correctly with 1 parameter', async () => {
+      let result = await client.postNonBlocking(COMMAND_NAME, 'param1');
 
-    expect(result).toBe(MOCK_RESULT);
-    expect(bridgeInstance.post).toBeCalledWith(
-      COMMAND_NAME,
-      ['param1', 'param2'],
-      { nonBlocking: true }
-    );
+      expect(result).toBe(MOCK_RESULT);
+      expect(bridgeInstance.post).toBeCalledWith(COMMAND_NAME, ['param1'], {
+        nonBlocking: true
+      });
+    });
+
+    test('should post correctly with 2 parameters', async () => {
+      let result = await client.postNonBlocking(
+        COMMAND_NAME,
+        'param1',
+        'param2'
+      );
+
+      expect(result).toBe(MOCK_RESULT);
+      expect(bridgeInstance.post).toBeCalledWith(
+        COMMAND_NAME,
+        ['param1', 'param2'],
+        { nonBlocking: true }
+      );
+    });
   });
 });
